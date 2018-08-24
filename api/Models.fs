4eded0b3ff388210b66782d8c55e4852a3488853
HennyH <hollingworth.henry@gmail.com>
2018-08-24 10:19:06 +0800

    (database) move configuration to Startup and complete university query function

--- api/Models.fs
+++ api/Models.fs
@@ -1,4 +1,4 @@
-namespace Exchange
+namespace API
 
 module Models =
 
--- api/Startup.fs
+++ api/Startup.fs
@@ -1,17 +1,15 @@
-namespace api
+namespace API
 
 open System
-open System.Collections.Generic
-open System.Linq
-open System.Threading.Tasks
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
-open Microsoft.AspNetCore.HttpsPolicy;
 open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
+open FSharp.Data.Dapper
 
 type Startup private () =
+
     new (configuration: IConfiguration) as this =
         Startup() then
         this.Configuration <- configuration
@@ -20,6 +18,7 @@ type Startup private () =
     member this.ConfigureServices(services: IServiceCollection) =
         // Add framework services.
         services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1) |> ignore
+        services.AddSingleton<IConfiguration>(this.Configuration) |> ignore
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     member this.Configure(app: IApplicationBuilder, env: IHostingEnvironment) =
@@ -30,5 +29,12 @@ type Startup private () =
 
         app.UseHttpsRedirection() |> ignore
         app.UseMvc() |> ignore
+        // This registers Option<'T> types with Dapper.
+        OptionHandler.RegisterTypes()
+        this.Configuration.["ConnectionString"] <-
+            match this.Configuration.["DATABASE"] with
+            | empty when String.IsNullOrEmpty(empty) -> (__SOURCE_DIRECTORY__ + "/bin/exchange.db")
+            | valid -> valid
+        printfn "Connection string = %s" this.Configuration.["ConnectionString"]
 
     member val Configuration : IConfiguration = null with get, set
