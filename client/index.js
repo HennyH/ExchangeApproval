25b6f99bd7b4b67a8eac6fe26cd39b33f604fd87
HennyH <hollingworth.henry@gmail.com>
2018-08-24 21:41:47 +0800

    (client) add CSS support to webpack

--- client/index.js
+++ client/index.js
@@ -1,4 +1,18 @@
 import m from 'mithril';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import classNames from 'classnames';
+import myStyles from './special.css'
 
 let root = document.body;
-m.render(root, <main><h1>Hello World!</h1></main>);
+m.render(
+    root,
+    <main>
+        <div class={myStyles.red}>
+            Hello World!
+        </div>
+        <div class={classNames("alert", "alert-primary")}>
+            Alert! Alert!
+        </div>
+    </main>
+);
