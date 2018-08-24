b0d96ceb5e87279c0ea923efe60e7e9ae7035d85
HennyH <hollingworth.henry@gmail.com>
2018-08-24 13:58:46 +0800

    (backend,database) unit decisions queries

---
+++ api/Enums.fs
@@ -0,0 +1,12 @@
+namespace API.Enums
+
+type UWAUnitContext =
+    | Elective = 1
+    | Core = 2
+    | Complementart = 3
+
+type UWAUnitLevel =
+    | One
+    | Two
+    | Three
+    | GreaterThanFour
