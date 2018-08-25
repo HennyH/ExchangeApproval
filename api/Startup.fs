73b9c3b4e15a265ce6c0b5c5ee84d1ca8b262c92
HennyH <hollingworth.henry@gmail.com>
2018-08-25 10:12:55 +0800

    (api) enable CORS

--- api/Startup.fs
+++ api/Startup.fs
@@ -10,6 +10,8 @@ open FSharp.Data.Dapper
 
 type Startup private () =
 
+    member val private enableCors = false with get, set
+
     new (configuration: IConfiguration) as this =
         Startup() then
         this.Configuration <- configuration
@@ -17,11 +19,29 @@ type Startup private () =
     // This method gets called by the runtime. Use this method to add services to the container.
     member this.ConfigureServices(services: IServiceCollection) =
         // Add framework services.
+
+        // Configure CORS so we can call the API from the client.
+        let corsOrigins = this.Configuration.["server.cors:origins"];
+        if not (String.IsNullOrEmpty(corsOrigins)) then
+            services.AddCors(
+                fun options ->
+                    options.AddPolicy(
+                        "FromAppSettings",
+                        fun builder -> builder.WithOrigins(corsOrigins.Split(";"))
+                                              .AllowAnyMethod()
+                                              .AllowAnyHeader() |> ignore
+                    ) |> ignore
+            ) |> ignore
+            this.enableCors <- true
+
         services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1) |> ignore
         services.AddSingleton<IConfiguration>(this.Configuration) |> ignore
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     member this.Configure(app: IApplicationBuilder, env: IHostingEnvironment) =
+        if this.enableCors then
+            app.UseCors("FromAppSettings") |> ignore
+
         if (env.IsDevelopment()) then
             app.UseDeveloperExceptionPage() |> ignore
         else
