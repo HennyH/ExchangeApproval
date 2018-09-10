8df537f16236be8524159081d1171f8cf5a10b9d
HennyH <hollingworth.henry@gmail.com>
2018-09-10 20:46:18 +0800

    (client) fix loading indicators

--- client/components/ApplicationPage/ApplicationPage.js
+++ client/components/ApplicationPage/ApplicationPage.js
@@ -2,25 +2,49 @@ import m from 'mithril'
 
 import Layout from 'Components/Layout'
 import ApplicationForm from './ApplicationForm'
+import Spinner from 'Components/Spinners/RectangularSpinner.js';
+
+const Data = {
+    filters: {
+        loading: false,
+        options: null,
+        fetch: () => {
+            Data.filters.loading = true;
+            m.request({
+                method: "GET",
+                url: "/api/requests/filters"
+            }).then(options => {
+                Data.filters.options = options;
+                Data.filters.loading = false;
+            });
+        }
+    }
+}
 
 export default function ApplicationPage() {
+
+    function oninit() {
+        Data.filters.fetch();
+    }
 
     function view() {
         return (
             <Layout>
-                <div class="container">
-                    <ApplicationForm
-                        contextTypeOptions={[
-                            { value: '1', label: 'Elective '},
-                            { value: '2', label: 'Core' }
-                        ]}
-                        electiveContextTypeOption={{ value: '1' }}
-                        changeCallback={console.log}
-                    />
-                </div>
+                {(Data.filters.loading
+                    ? <Spinner style="top: calc(50% - 32px); left: calc(50% - 32px); position: absolute;" />
+                    : (
+                        <div class="container">
+                            <ApplicationForm
+                                contextTypeOptions={Data.filters.options.uwaUnitContextOptions.map(n => ({ value: n, label: n}))}
+                                electiveContextTypeOption={{ value: 'Elective' }}
+                                changeCallback={console.log}
+                            />
+                        </div>
+                    )
+                )}
             </Layout>
         )
     }
 
-    return { view }
+    return { oninit, view }
 }
