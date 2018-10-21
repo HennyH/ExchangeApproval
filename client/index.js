425633b293d26fd9ff9ac79d9494651231b89b98
HennyH <hollingworth.henry@gmail.com>
2018-10-21 18:54:38 +0800

    (auth) add authentication to client

--- client/index.js
+++ client/index.js
@@ -3,18 +3,15 @@ import m from 'mithril';
 import SearchPage from 'Components/SearchPage';
 import ApplicationPage from 'Components/ApplicationPage';
 import StudentOfficePage from 'Components/StaffPage';
-import Login from 'Components/Login';
 
 (() => {
     m.route(document.body, "/search", {
         "/search": SearchPage,
         "/application": ApplicationPage,
         "/student-office": {
-			onmatch: function() {
-				if (!window.isLoggedIn) m.route.set("/login")
-				else return StudentOfficePage
-			}
-		},
-		"/login": Login
+            onmatch: function () {
+                return m.request("/login").then(() => StudentOfficePage, (() => SearchPage));
+            }
+        }
     });
 })()
