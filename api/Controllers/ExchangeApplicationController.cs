fcba63d79654c3b6c2269b9865a656382ac2d121
HennyH <hollingworth.henry@gmail.com>
2018-10-22 22:35:45 +0800

    (api) rudimentary submit application

---
+++ api/Controllers/ExchangeApplicationController.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ExchangeApproval.Data;
+using ExchangeApproval.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeApproval.Controllers
+{
+    [Route("/application")]
+    public class ExchangeApplicationController : Controller
+    {
+        private readonly ExchangeDbContext _db;
+
+        public ExchangeApplicationController(ExchangeDbContext db)
+        {
+            this._db = db;
+        }
+
+        [HttpPost]
+        public ActionResult SubmitApplication([FromBody]ApplicationFormVM form)
+        {
+            Console.WriteLine(form);
+            var now = DateTime.UtcNow;
+            var application = new StudentApplication
+            {
+                SubmittedAt = now,
+                LastUpdatedAt = now,
+                CompletedAt  = now,
+                StudentName = form.StudentDetailsForm.Name,
+                StudentNumber = form.StudentDetailsForm.Email,
+                Major1st = form.StudentDetailsForm.Major,
+                Major2nd = form.StudentDetailsForm.Major2nd,
+                ExchangeUniversityCountry = null,
+                ExchangeUniversityName  = form.ExchangeUniversityForm.UniversityName,
+                ExchangeUniversityHref = form.ExchangeUniversityForm.UniversityHomepage,
+                Notes = null,
+                UnitSets = form.UnitSetForms.Select(f => new UnitSet
+                {
+                    ExchangeUniversityName  = form.ExchangeUniversityForm.UniversityName,
+                    ExchangeUniversityHref = form.ExchangeUniversityForm.UniversityHomepage,
+                    SubmittedAt = now,
+                    LastUpdatedAt = now,
+                    CompletedAt  = now,
+                    IsEquivalent = null,
+                    IsContextuallyApproved = null,
+                    EquivalentUWAUnitLevel = null,
+                    ExchangeUnits = f.ExchangeUnitsForm.Select(u => new ExchangeUnit
+                    {
+                        Code = u.UnitCode,
+                        Title = u.UnitName,
+                        Href = u.UnitHref
+                    }).ToList(),
+                    UWAUnits = f.UWAUnitsFormForm?.Select(u => new UWAUnit
+                    {
+                        Code = u.UnitCode,
+                        Title = u.UnitName,
+                        Href = u.UnitHref
+                    }).ToList() ?? new List<UWAUnit>(),
+                }).ToList()
+            };
+            this._db.Add(application);
+            this._db.SaveChanges();
+            return Json(form);
+        }
+    }
+}
