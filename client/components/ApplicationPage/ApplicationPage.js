5d85dae0eb403a6628f92f331f145f0e3019dab5
Jdjellis <joshua.ellis@iinet.net.au>
2018-10-22 13:32:13 +0800

    Fixed ApplicationPage double header

--- client/components/ApplicationPage/ApplicationPage.js
+++ client/components/ApplicationPage/ApplicationPage.js
@@ -2,7 +2,8 @@ import m from 'mithril'
 
 import Layout from 'Components/Layout'
 import ApplicationForm, { ApplicationPowerForm } from './ApplicationForm'
-import {ApplicationData} from '../ViewData'
+import Spinner from 'Components/Spinners/RectangularSpinner.js';
+import {ApplicationData, UnitSearchData as Data} from '../ViewData'
 
 const applicationPowerForm = new ApplicationPowerForm({
 	staffView: false,
@@ -15,35 +16,34 @@ function showData() {
 
 export default function ApplicationPage() {
 
+    function oninit() {
+        Data.filters.fetch();
+    }
+
     function view() {
         return (
-            <Layout>
-				<div class="container-fluid">
-				{!ApplicationData.hasSubmitted ? 
-					(<div class="card my-3">
-						<div class="card-header">
-							Exchange Application
-						</div>
-						<ApplicationForm form={applicationPowerForm} staffView ={false} />
-					</div>)
-				:	(<div class="card my-3">
-						<div class="card-header">
-							Application complete!
-						</div>
-						<div class="card-body text-center">
-							<h6>Thank you for your application. </h6> 
-							<br/>
-							Your allocated student office staff will process the application and get back to you soon!
-							<br/>
-							<br/>
-							<em>Please check your student email regularly.</em>
-						</div>
-					</div>)
-				}
-				</div>
-            </Layout>
+        <Layout>
+            {ApplicationData.hasSubmitted ? 
+            <div class="card my-3">
+                <div class="card-header">Application complete!</div>
+                <div class="card-body text-center">
+                    <h6>Thank you for your application. </h6> 
+                    <br/>Your allocated student office staff will process the application and get back to you soon!<br/><br/>
+                    <em>Please check your student email regularly.</em>
+                </div>
+            </div>
+            : (Data.filters.loading ? <Spinner style="top: calc(50% - 32px); left: calc(50% - 32px); position: absolute;" />
+            : <div class="container-fluid">
+                <div class="card my-3">
+                    <div class="card-header"> Exchange Application</div>
+                    <ApplicationForm form={applicationPowerForm} staffView ={false} />
+                </div>
+            </div>
+            )}
+        </Layout>
         )
     }
 
-    return { view }
+
+    return { oninit, view }
 }
