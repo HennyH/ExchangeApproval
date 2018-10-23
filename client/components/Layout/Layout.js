4766c57e6763c00672e59f28b8be51e4f2956b47
HennyH <hollingworth.henry@gmail.com>
2018-10-23 11:12:40 +0800

    (client) remove logo

--- client/components/Layout/Layout.js
+++ client/components/Layout/Layout.js
@@ -15,7 +15,6 @@ var Navbar = {
         return(
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <a class="navbar-brand" href="#">
-                    <div class={classNames(Styles.uwaLogo, "d-inline-block", "align-middle")}></div>
                     UWA Exchange Helper
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapsable" aria-controls="navbarCollapsable" aria-expanded="false" aria-label="Toggle navigation">
