816c4bd12ceb94f1988a37d401d6dc059692205d
HennyH <hollingworth.henry@gmail.com>
2018-10-23 10:47:43 +0800

    (api) add inbox API

---
+++ api/ViewModels/StaffInboxItemVM.cs
@@ -0,0 +1,15 @@
+using System;
+using ExchangeApproval.Data;
+
+namespace ExchangeApproval.ViewModels
+{
+    public class StaffInboxItemVM
+    {
+        public int StudentApplicationId { get; set; }
+        public string StudentName { get; set; }
+        public string StudentNumber { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+        public string ExchangeUniversity { get; set; }
+        public SelectOption<StudentApplicationStatus> StudentApplicationStatus { get; set; }
+    }
+}
