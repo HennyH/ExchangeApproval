b4d3bcefa238263deb81d765c6a7c515b494d433
HennyH <hollingworth.henry@gmail.com>
2018-10-31 11:54:16 +0800

    (client) show messages in correct place in admin modal

--- client/components/StaffPage/AdminToolsModal.js
+++ client/components/StaffPage/AdminToolsModal.js
@@ -5,8 +5,10 @@ import Modal from '../Modal/Modal.js';
 export default function AdminToolsModal() {
 
     const state = {
-        errors: [],
-        message: null
+        equivalenciesMessage: null,
+        equivalenciesErrors: [],
+        applicationsMessage: null,
+        applicationsErrors: []
     }
 
     function uploadNewEquivalencies(event) {
@@ -19,12 +21,12 @@ export default function AdminToolsModal() {
             data
         })
         .then(() => {
-            state.message = "Upload Successful.";
-            state.errors = [];
+            state.equivalenciesMessage = "Upload Successful.";
+            state.equivalenciesErrors = [];
         })
         .catch(e => {
-            state.message = null;
-            state.errors = JSON.parse(e.message);
+            state.equivalenciesMessage = null;
+            state.equivalenciesErrors = JSON.parse(e.message);
             m.redraw();
         });
     }
@@ -39,12 +41,12 @@ export default function AdminToolsModal() {
             data
         })
         .then(() => {
-            state.message = "Upload Successful.";
-            state.errors = [];
+            state.applicationsMessage = "Upload Successful.";
+            state.applicationsErrors = [];
         })
         .catch(e => {
-            state.message = null;
-            state.errors = JSON.parse(e.message);
+            state.applicationsMessage = null;
+            state.applicationsErrors = JSON.parse(e.message);
             m.redraw();
         });
     }
@@ -61,40 +63,66 @@ export default function AdminToolsModal() {
                 {...attrs}
             >
                 <div>
-                    <h6 class="text-muted">Edit Unit Equivalencies</h6>
-                        <div class="form-group">
-                            <a href="/api/admin/equivalencies" class="btn btn-outline-primary form-control-file" target="_blank" download="equivalencies.csv">
-                                {"⇩ Download Equivalencies"}
-                            </a>
-                        </div>
-                        <div class="form-group">
-                            <label class="btn btn-outline-danger form-control-file">
-                                {"⇧ Upload Equivalencies"}
-                                <input name="equivalencies" type="file" onchange={uploadNewEquivalencies} hidden />
-                            </label>
-                        </div>
-                        {state.message ? <h6>{state.message}</h6> : <span />}
-                        {state.errors
-                            ? (
-                                <ol>
-                                    {state.errors.map(e => <li class="small text-danger" key={e}>{e}</li>)}
-                                </ol>
-                            )
-                            : <span />
-                        }
-                        <hr/>
-                        <h6 class="text-muted">Backup Database</h6>
-                        <div class="form-group">
-                            <a href="/api/admin/applications" class="btn btn-outline-primary form-control-file" target="_blank" download="ExchangeApprovalsDB.json">
-                                {"⇩ Download Applications"}
-                            </a>
-                        </div>
-                        <div class="form-group">
-                            <label class="btn btn-outline-danger form-control-file">
-                                {"⇧ Upload Applications"}
-                                <input name="applications" type="file" onchange={uploadNewDB} hidden />
-                            </label>
-                        </div>
+                    <h6 class="text-muted">Edit Historical Unit Equivalencies</h6>
+                    <p>
+                        This tool allows you to edit unit equivalencies that are <i>not</i> related
+                        to a student application by downloading and uploading a CSV. This is useful when you have historical
+                        equivalency data you wish to import into the system.
+                        <br />
+                        <span class="text-danger">
+                        Warning: Uploading the file will <b>remove all previously uploaded historical equivalencies</b> from
+                        the database, and insert new data for each equivalency in the CSV file.</span>
+                    </p>
+                    <div class="form-group">
+                        <a href="/api/admin/equivalencies" class="btn btn-outline-primary form-control-file" target="_blank" download="equivalencies.csv">
+                            {"⇩ Download Historical Equivalencies"}
+                        </a>
+                    </div>
+                    <div class="form-group">
+                        <label class="btn btn-outline-danger form-control-file">
+                            {"⇧ Upload Historical Equivalencies"}
+                            <input name="equivalencies" type="file" onchange={uploadNewEquivalencies} hidden />
+                        </label>
+                    </div>
+                    {state.equivalenciesMessage ? <h6>{state.equivalenciesMessage}</h6> : <span />}
+                    {state.equivalenciesErrors
+                        ? (
+                            <ol>
+                                {state.equivalenciesErrors.map(e => <li class="small text-danger" key={e}>{e}</li>)}
+                            </ol>
+                        )
+                        : <span />
+                    }
+                    <hr/>
+                    <h6 class="text-muted">Backup Database</h6>
+                    <p>
+                        This tool allows you to download all the student applications into a JSON file. This file can
+                        be edited using a text editor such as Notepad++ or Sublime Text on Windows and TextEdit on Mac.
+                        <br />
+                        <span class="text-danger">
+                        Warning: Uploading the file will <b>remove all existing applications</b> from
+                        the database, and insert new data for each application in the JSON file.</span>
+                    </p>
+                    <div class="form-group">
+                        <a href="/api/admin/applications" class="btn btn-outline-primary form-control-file" target="_blank" download="ExchangeApprovalsDB.json">
+                            {"⇩ Download Applications"}
+                        </a>
+                    </div>
+                    <div class="form-group">
+                        <label class="btn btn-outline-danger form-control-file">
+                            {"⇧ Upload Applications"}
+                            <input name="applications" type="file" onchange={uploadNewDB} hidden />
+                        </label>
+                    </div>
+                    {state.applicationsMessage ? <h6>{state.applicationsMessage}</h6> : <span />}
+                    {state.applicationsErrors
+                        ? (
+                            <ol>
+                                {state.applicationsErrors.map(e => <li class="small text-danger" key={e}>{e}</li>)}
+                            </ol>
+                        )
+                        : <span />
+                    }
                 </div>
             </Modal>
         );
