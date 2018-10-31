839e6bee24b13a54e066c3a307f5a094eda6f1e0
HennyH <hollingworth.henry@gmail.com>
2018-10-31 12:00:04 +0800

    (api) remove test data

--- api/Startup.cs
+++ api/Startup.cs
@@ -133,54 +133,6 @@ namespace ExchangeApproval
                 else
                 {
                     db.Database.Migrate();
-                    db.Add(new StudentApplication
-                    {
-                        SubmittedAt = DateTime.Now,
-                        LastUpdatedAt = DateTime.Now,
-                        StudentName = "Henry Hollingworth " + new Random().Next().ToString(),
-                        StudentNumber = "21471423",
-                        Major1st = "Computer Science",
-                        ExchangeUniversityCountry = "Finalnd",
-                        ExchangeUniversityHref = "http://finalnd.com",
-                        ExchangeUniversityName = "Finland University",
-                        UnitSets = new UnitSet[]
-                        {
-                            new UnitSet
-                            {
-                                ExchangeUniversityCountry = "Finalnd",
-                                ExchangeUniversityHref = "http://finalnd.com",
-                                ExchangeUniversityName = "Finland University",
-                                IsContextuallyApproved = true,
-                                IsEquivalent = null,
-                                EquivalentUWAUnitLevel = UWAUnitLevel.Two,
-                                ExchangeUnits = new ExchangeUnit[]
-                                {
-                                    new ExchangeUnit
-                                    {
-                                        Code = "MATH-HARD",
-                                        Title = "The Hardest Math",
-                                        Href = "http://hard-math.com"
-                                    }
-                                },
-                                UWAUnits = new UWAUnit[]
-                                {
-                                    new UWAUnit
-                                    {
-                                        Code = "MATH-1001",
-                                        Title = "Mathematics I",
-                                        Href = "http://uwa-unit.com"
-                                    },
-                                    new UWAUnit
-                                    {
-                                        Code = "MATH-1002",
-                                        Title = "Mathematics II",
-                                        Href = "http://uwa-unit.com"
-                                    }
-                                }
-                            }
-                        }
-                    });
-                    db.SaveChanges();
                 }
             }
         }
